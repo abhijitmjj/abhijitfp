2d08d5ec77cb2012229ba285efccf5f1988df51a
Abhijit Gupta <abhijit038@gmail.com>
2023-04-02 02:15:25 +0530

    added code

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
-    
+
 -- all functions are there in the Lib
 import Lib
 
 main :: IO ()
 main = do
        someFunc
+       -- convert list[String] to String
+       putStrLn $ concatMap (\x -> show x ++ "\n") (solutions' [1..5] 15)
+       -- print (map show $ solutions' [1..5] 15)
        return ()
